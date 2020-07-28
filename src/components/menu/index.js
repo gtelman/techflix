3c8f6be1b08ebb8b8e5d7283c82a4248c64bdd1b
gtelman <gabrielletelman@gmail.com>
2020-07-28 16:48:08 -0300

    Setup inicial do projeto

---
+++ src/components/menu/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Logo from '../../assets/img/Logo.png'
+import './Menu.css' 
+//import ButtonLink from '../ButtonLink';
+import Button from '../Button';
+
+function Menu(){
+    return (
+        <nav className="Menu">
+            <a href="/">
+                <img className="Logo" src={Logo} atl="Youtech logo"/>
+            </a>
+            <Button as="a" className="ButtonLink" href="/">
+                Novo Vídeo
+            </Button>
+        </nav>
+    );
+}
+
+export default Menu;
