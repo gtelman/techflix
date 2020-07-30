042758a5e9dcd1b1012ef829ca4bac0ee3a4465c
gtelman <gabrielletelman@gmail.com>
2020-07-30 00:20:05 -0300

    including category and new video pages

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,16 +1,17 @@
-import React from 'react'
-import Logo from '../../assets/img/Logo.png'
-import './Menu.css' 
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/img/Logo.png';
+import './Menu.css'; 
 //import ButtonLink from '../ButtonLink';
 import Button from '../Button';
 
 function Menu(){
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={Logo} atl="Youtech logo"/>
-            </a>
-            <Button as="a" className="ButtonLink" href="/">
+            </Link>
+            <Button as={Link} className="ButtonLink" to="cadastro/video">
                 Novo Vídeo
             </Button>
         </nav>
